f67eac826b9b14c025eee09c0ec01330d8e50762
Steinunn <stemfri@gmail.com>
2014-10-14 18:02:41 +0000

    added newline possibilty

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -18,7 +18,9 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+		String regexp = "[,\\n]";
+		
+		return numbers.split(regexp);
 	}
       
     private static int sum(String[] numbers){
